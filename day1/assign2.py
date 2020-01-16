d763a0a6f52fceecd63a6abb71ba63806815b9f7
ap1124 <patelabhi1124@gmail.com>
2020-01-16 10:19:47 +0530

    Final commit

--- day1/assign2.py
+++ day1/assign2.py
@@ -1,6 +1,11 @@
-str1="90"
-print(str1)
-str1=str1.ljust(15,'$')
-str1=str1.rjust(30,'$')
+str1 = input("Enter a string: ")
+str2 = input("Enter an another string: ")
 
-print(str1)
+str3 = str1 +" "+str2
+
+print(f"To upper: {str3.upper()} \nTo Lower: {str3.lower()} \nTitle case: {str3.title()}")
+
+str4 = "String"
+str4 = str4.join(str1.split())
+
+print(str4.center(30, '$'))
